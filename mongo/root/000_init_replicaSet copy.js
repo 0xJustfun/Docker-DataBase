c2fb4e93f43326a3c775bb9958af3d60955107a2
0xJustfun <57807165+0xJustfun@users.noreply.github.com>
2020-12-19 21:04:45 +0800

    feat:修复链接

--- mongo/root/000_init_replicaSet copy.js
+++ mongo/root/000_init_replicaSet copy.js
@@ -3,8 +3,8 @@ rs.initiate(
         _id: "rs0",
         members: [
             {_id: 0, host: "107.174.206.37:27017"},
-            {_id: 1, host: "107.174.206.37:27017"},
-            {_id: 2, host: "107.174.206.37:27017", arbiterOnly: true}
+            {_id: 1, host: "107.174.206.37:27018"},
+            {_id: 2, host: "107.174.206.37:27019", arbiterOnly: true}
         ]
     }
 );
--- mysql/root/sync.sh
+++ mysql/root/sync.sh
@@ -2,7 +2,7 @@ FROM_HOST=34.92.198.209
 FROM_U=root
 FROM_P=gRrcYm07lj7Q
 
-TO_HOST=192.168.88.28
+TO_HOST=127.0.0.1
 TO_U=root
 TO_P=gRrcYm07lj7Q
 
@@ -10,3 +10,7 @@ TO_P=gRrcYm07lj7Q
 # 不能自动输入密码，会出错
 echo "mysqldump -h ${FROM_HOST} -u ${FROM_U} -p --all-databases > /var/lib/mysql/all.sql"
 echo "mysql -h ${TO_HOST} -u ${TO_U} -p < /var/lib/mysql/all.sql"
+
+
+# docker-compose exec mysql /bin/sh
+# sh /root/sync.sh
